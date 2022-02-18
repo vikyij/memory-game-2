cc69aed4d7619e34b11f37610efc8ff2f985b7e0
vikyij <vikkyij54@gmail.com>
2022-02-18 20:40:07 +0100

    install typrscript,tailwind

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+
+const Home = () => {
+  return <h1 className='bg-dark-blue text-white font-bold underline'>Home</h1>
+}
+export default Home
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  colors: {
+    'dark-blue': '#152938',
+    white: '#ffffff',
+  },
+  plugins: [],
+}
